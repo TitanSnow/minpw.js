179d62744916b082ea82a7e549e5711a3c640173
TitanSnow <sweeto@live.cn>
2017-01-30 12:02:53 +0800

    add pygame.transform.laplacian&mpwGrayscale

--- minpw.js
+++ minpw.js
@@ -702,6 +702,22 @@
 						suf_dest.context.clearRect(rect.x,rect.y,rect.w,rect.h)
 						return suf_dest
 					}
+					this.laplacian=function(suf,suf_dest){
+						if(arguments.length==1)
+							suf_dest=pygame.Surface([suf.width,suf.height])
+						var px=suf.context.getImageData(0,0,suf.width,suf.height)
+						px=lib.lena.laplacian(px)
+						suf_dest.context.putImageData(px,0,0)
+						return suf_dest
+					}
+					this.mpwGrayscale=function(suf,suf_dest){
+						if(arguments.length==1)
+							suf_dest=pygame.Surface([suf.width,suf.height])
+						var px=suf.context.getImageData(0,0,suf.width,suf.height)
+						px=lib.lena.grayscale(px)
+						suf_dest.context.putImageData(px,0,0)
+						return suf_dest
+					}
 				}
 			}
 		}
@@ -956,4 +972,87 @@ function BlurStack()
 	this.a = 0;
 	this.next = null;
 }
+})(),lena:(function(){
+/*
+ *  lena-js - v0.0.1
+ *  Library for image processing
+ *  https://github.com/davidsonfellipe/lena-js/
+ *
+ *  Made by Davidson Fellipe
+ *  Under MIT License
+ */
+var LenaJS = {};
+LenaJS.convolution = function(pixels, weights) {
+
+  var side = Math.round(Math.sqrt(weights.length)),
+      halfSide = Math.floor(side/2),
+      src = pixels.data,
+      canvasWidth = pixels.width,
+      canvasHeight = pixels.height,
+      temporaryCanvas = document.createElement('canvas'),
+      temporaryCtx = temporaryCanvas.getContext('2d'),
+      outputData = temporaryCtx.createImageData(canvasWidth, canvasHeight);
+
+  for (var y = 0; y < canvasHeight; y++) {
+
+    for (var x = 0; x < canvasWidth; x++) {
+
+      var dstOff = (y * canvasWidth + x) * 4,
+          sumReds = 0,
+          sumGreens = 0,
+          sumBlues = 0,
+          sumAlphas = 0;
+
+      for (var kernelY = 0; kernelY < side; kernelY++) {
+        for (var kernelX = 0; kernelX < side; kernelX++) {
+
+          var currentKernelY = y + kernelY - halfSide,
+              currentKernelX = x + kernelX - halfSide;
+
+          if (currentKernelY >= 0 &&
+              currentKernelY < canvasHeight &&
+              currentKernelX >= 0 &&
+              currentKernelX < canvasWidth) {
+
+            var offset = (currentKernelY * canvasWidth + currentKernelX) * 4,
+                weight = weights[kernelY * side + kernelX];
+
+            sumReds += src[offset] * weight;
+            sumGreens += src[offset + 1] * weight;
+            sumBlues += src[offset + 2] * weight;
+          }
+        }
+      }
+
+      outputData.data[dstOff] = sumReds;
+      outputData.data[dstOff+1] = sumGreens;
+      outputData.data[dstOff+2] = sumBlues;
+      outputData.data[dstOff+3] = 255;
+    }
+  }
+  return outputData;
+};
+LenaJS.grayscale = function(pixels, args) {
+
+  for (var i = 0; i < pixels.data.length; i += 4) {
+
+    var r = pixels.data[i],
+        g = pixels.data[i+1],
+        b = pixels.data[i+2];
+
+    pixels.data[i] = pixels.data[i+1] = pixels.data[i+2] = 0.2126*r + 0.7152*g + 0.0722*b;
+
+  }
+
+  return pixels;
+};
+LenaJS.laplacian = function(pixels, args) {
+
+  var operator = [ 0, -1, 0,
+                  -1, 4, -1,
+                  0, -1, 0 ];
+
+  return LenaJS.convolution(pixels, operator);
+};
+return LenaJS;
 })()});
